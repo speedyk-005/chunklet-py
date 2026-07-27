0e616b419a9d858a05015500b83ed9b403299868
speedyk-005 <speedy40115719@gmail.com>
2026-07-27 14:07:18 -0400

    chore: use uv for build_docs.sh commands

--- build_docs.sh
+++ build_docs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build the Python package
-python -m build
+uv build
 
 # Set PYTHONPATH to include the src directory for mkdocstrings
 export PYTHONPATH=$(pwd)/src:$PYTHONPATH
 
 # Build the MkDocs documentation
-python -m mkdocs build
+uv python -m mkdocs build
 
 # Deploy the documentation using mike
-mike deploy --push --update-aliases 2.4.0 latest
+uv run mike deploy --push --update-aliases 2.4.0 latest
