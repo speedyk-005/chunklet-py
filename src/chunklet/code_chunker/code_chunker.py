b5ef23e409b57d39e0a249b1876b301f4c1c7143
speedyk-005 <speedy40115719@gmail.com>
2026-07-29 11:43:06 -0400

    refactor(code_chunker): simplify limit check with any()

--- src/chunklet/code_chunker/code_chunker.py
+++ src/chunklet/code_chunker/code_chunker.py
@@ -398,9 +398,7 @@ class CodeChunker(BaseChunker):
                 function_count + 1 > max_functions
             )
 
-            if not (
-                token_limit_reached or line_limit_reached or function_limit_reached
-            ):
+            if not any([token_limit_reached, line_limit_reached, function_limit_reached]):
                 # Fits: merge normally
                 merged_content.append(snippet_dict["content"])
                 relations_list.append(snippet_dict["relations"])
